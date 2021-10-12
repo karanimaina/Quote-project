2fff878899a47e46598d499166e2949f4524da73
felixmaina <karani.maina2010@gmail.com>
2021-10-12 13:27:11 +0300

    modify app component .html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Quote } from './quote'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,9 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Quotes';
-  quotes!:string[];
+  quotes:Quote[] = [
+    {id:1, author:'Khalifatul Masih III',description:"Love For All, Hatred For None."},
+    {id:2, author:'Amy Poehler',description:"Every moment is a fresh beginning."},
+    {id:3, author:'T.S Eliot',description:"Die with memories, not dreams. "},
+    {id:4, author:'Mark Twain',description:"Never regret anything that made you smile."},
+    {id:5, author:'Pablo Picasso',description:"Everything you can imagine is real. "},
+    {id:6, author:' Leonardo da Vinci',description:"Simplicity is the ultimate sophistication"},
 
-constructor(){
-  this.quotes =[ "Change the world by being yourself.","Every moment is a fresh beginning.","Never regret anything that made you smile","Love For All, Hatred For None. "] 
-}
+  ]
 }
