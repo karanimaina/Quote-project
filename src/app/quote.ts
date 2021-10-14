717e87488968990c5e58b8ee3bd418adbbbdb569
felixmaina <karani.maina2010@gmail.com>
2021-10-14 03:25:12 +0300

    Auto-generated commit [ci skip]

--- src/app/quote.ts
+++
@@ -1,7 +0,0 @@
-export class Quote {
-    showDescription: boolean;
-  name: any;
-    constructor(public id: number,public author: string,public description: string,public publisher: string, public completeDate: Date){
-      this.showDescription=false;
-    }
-  }  
